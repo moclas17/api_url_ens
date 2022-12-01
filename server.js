cd00a5e44a9e83757ed31766904b93b7a28577b9
Erik Valle <erik.valle@gmail.com>
2022-12-01 11:19:29 -0600

    server
    add server

---
+++ server.js
@@ -0,0 +1,31 @@
+const { query } = require('express');
+const express = require('express');
+const app = express();
+
+var ethers = require('ethers');
+var url = 'https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161';
+var provider = new ethers.providers.JsonRpcProvider(url);
+
+async function getlink(ens) {
+    const resolver = await provider.getResolver(ens);
+    const linkmetadata = await resolver.getText("url");
+    console.log(linkmetadata); // pero en la consola si se muestra lo que quiero :D 
+    return linkmetadata;
+}
+
+app.get('/', async (req, res) => {
+    console.log(req.query.data);
+    let domain=req.query.data;
+    let miurl = await getlink(domain+".eth");
+    res.send(domain + " :  "+ miurl); //sabes por que miurl regresa [object Promise] en el navegador
+});
+
+// Listen to the App Engine-specified port, or 8080 otherwise
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
+
+
+
+
